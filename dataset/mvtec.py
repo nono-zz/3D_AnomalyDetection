4dd3d01212a99e4cc43b2222142dbcd967fde346
Josh <zhaoxia2@ualberta.ca>
2022-08-08 21:21:48 -0600

    nothing

--- dataset/mvtec.py
+++ dataset/mvtec.py
@@ -9,45 +9,27 @@ import numpy as np
 CLASS_NAMES  = ['bagel', 'cable_gland', 'carrot', 'cookie', 'dowel', 'foam', 'peach', 'potato', 'rope', 'tire']
 
 class MVTecDataset(Dataset):
-    def __init__(self, dataset_path, class_name='bottle', is_train=True, resize=256, cropsize=224, wild_ver=False):
+    def __init__(self, dataset_path, class_name='bottle', is_train=True, resize=256):
         assert class_name in CLASS_NAMES, 'class_name: {}, should be in {}'.format(class_name, CLASS_NAMES)
         self.dataset_path = dataset_path
         self.class_name = class_name
         self.is_train = is_train
         self.resize = resize
-        self.cropsize = cropsize
 
         self.rgb_paths, self.xyz_paths, self.y, self.mask = self.load_dataset_folder()
+        
+        self.transform_x =   T.Compose([T.Resize(resize, Image.ANTIALIAS),
+                                        T.ToTensor(),
+                                        T.Normalize(mean=[0.485, 0.456, 0.406],
+                                                    std=[0.229, 0.224, 0.225])])
 
-        if wild_ver:
-            self.transform_x =   T.Compose([T.Resize(resize, Image.ANTIALIAS),
-                                            T.RandomRotation(10),
-                                            T.RandomCrop(cropsize),
-                                            T.ToTensor(),
-                                            T.Normalize(mean=[0.485, 0.456, 0.406],
-                                                        std=[0.229, 0.224, 0.225])])
-
-            self.transform_mask =    T.Compose([T.Resize(resize, Image.NEAREST),
-                                                T.RandomRotation(10),
-                                                T.RandomCrop(cropsize),
-                                                T.ToTensor()])
-                                                
-        else:
-            self.transform_x =   T.Compose([T.Resize(resize, Image.ANTIALIAS),
-                                            T.CenterCrop(cropsize),
-                                            T.ToTensor(),
-                                            T.Normalize(mean=[0.485, 0.456, 0.406],
-                                                        std=[0.229, 0.224, 0.225])])
-
-            self.transform_mask =    T.Compose([T.Resize(resize, Image.NEAREST),
-                                                T.CenterCrop(cropsize),
-                                                T.ToTensor()])
-            
-            self.transform_FPFH = T.Compose([T.Resize(resize, Image.ANTIALIAS),
-                                            T.CenterCrop(cropsize),
-                                            T.ToTensor(),
-                                            T.Normalize(mean=[0.456],
-                                                        std=[0.224])])
+        self.transform_mask =    T.Compose([T.Resize(resize, Image.NEAREST),
+                                            T.ToTensor()])
+        
+        self.transform_FPFH = T.Compose([T.Resize(resize, Image.ANTIALIAS),
+                                        T.ToTensor(),
+                                        T.Normalize(mean=[0.456],
+                                                    std=[0.224])])
 
     def __getitem__(self, idx):
         # x, y, mask = self.x[idx], self.y[idx], self.mask[idx]
@@ -123,3 +105,104 @@ class MVTecDataset(Dataset):
 
         # return list(x), list(y), list(mask)
         return list(rgb_tot_fpath_list), list(xyz_tot_fpath_list), list(y), list(mask)
+
+
+class MVTec_RGBD_Dataset(Dataset):
+    def __init__(self, dataset_path, class_name='bottle', is_train=True, resize=256):
+        assert class_name in CLASS_NAMES, 'class_name: {}, should be in {}'.format(class_name, CLASS_NAMES)
+        self.dataset_path = dataset_path
+        self.class_name = class_name
+        self.is_train = is_train
+        self.resize = resize
+
+        self.rgb_paths, self.depth_paths, self.y, self.mask = self.load_dataset_folder()
+        
+        self.transform_x =   T.Compose([T.Resize(resize, Image.ANTIALIAS),
+                                        T.ToTensor(),
+                                        T.Normalize(mean=[0.485, 0.456, 0.406],
+                                                    std=[0.229, 0.224, 0.225])])
+
+        self.transform_mask =    T.Compose([T.Resize(resize, Image.NEAREST),
+                                            T.ToTensor()])
+        
+        self.transform_FPFH = T.Compose([T.Resize(resize, Image.ANTIALIAS),
+                                        T.ToTensor(),
+                                        T.Normalize(mean=[0.456],
+                                                    std=[0.224])])
+
+    def __getitem__(self, idx):
+        rgb_path, depth_path, label, mask = self.rgb_paths[idx], self.depth_paths[idx], self.y[idx], self.mask[idx]
+        
+        # read rgb
+        rgb_img = Image.open(rgb_path).convert('RGB')
+        rgb_img = self.transform_x(rgb_img)
+        
+        # read depth
+        depth_img = Image.open(depth_path).convert('RGB')
+        depth_img = self.transform_x(rgb_img) 
+        
+        
+        
+        if label == 0:
+            mask = torch.zeros([1, self.cropsize, self.cropsize])
+        else:
+            mask = Image.open(mask)
+            mask = self.transform_mask(mask)
+        
+    
+        return rgb_img
+    
+
+    def __len__(self):
+        return len(self.rgb_paths)
+
+    def load_dataset_folder(self):
+        phase = 'train' if self.is_train else 'test'
+        # x, y, mask = [], [], []
+        rgb_tot_fpath_list, depth_tot_fpath_list, y, mask = [], [], [], []
+
+        img_dir = os.path.join(self.dataset_path, self.class_name, phase)
+        # gt_dir = os.path.join(self.dataset_path, self.class_name, 'gt')
+
+        img_types = sorted(os.listdir(img_dir))
+        for img_type in img_types:
+
+            img_type_dir = os.path.join(img_dir, img_type)
+            if not os.path.isdir(img_type_dir):
+                continue
+            
+            rgb_type_dir = os.path.join(img_type_dir, 'rgb')
+            depth_type_dir = os.path.join(img_type_dir, 'depth')
+            
+            # read RGB
+            rgb_fpath_list = sorted([os.path.join(rgb_type_dir, f)
+                                    for f in os.listdir(rgb_type_dir)
+                                    if f.endswith('.png')])
+            
+            rgb_tot_fpath_list.extend(rgb_fpath_list)
+            
+            # read depth
+            depth_type_dir = sorted([os.path.join(depth_type_dir, f)
+                                    for f in os.listdir(depth_type_dir)
+                                    if f.endswith('.png')])
+            
+            depth_tot_fpath_list.extend(depth_type_dir)
+            
+
+            if img_type == 'good':
+                y.extend([0] * len(rgb_fpath_list))
+                mask.extend([None] * len(rgb_fpath_list))
+            else:
+                gt_type_dir = os.path.join(img_dir, img_type, 'gt')
+                y.extend([1] * len(rgb_fpath_list))
+                # gt_type_dir = os.path.join(gt_dir, img_type)
+                img_fname_list = [os.path.splitext(os.path.basename(f))[0] for f in rgb_fpath_list]
+                gt_fpath_list = [os.path.join(gt_type_dir, img_fname + '.png')
+                                for img_fname in img_fname_list]
+                mask.extend(gt_fpath_list)
+
+        assert len(rgb_tot_fpath_list) == len(y), 'number of x and y should be same'
+        assert len(rgb_tot_fpath_list) == len(depth_tot_fpath_list), 'length of rgb and depth is not the same'
+
+        # return list(x), list(y), list(mask)
+        return list(rgb_tot_fpath_list), list(depth_tot_fpath_list), list(y), list(mask)
--- train.py
+++ train.py
@@ -1,21 +1,16 @@
 import torch
 from torch.utils.data import DataLoader
 from torch import optim
+import torch.nn.functional as F
 import numpy as np
-
-import torch.nn.MSELoss as MSEloss
-
-from models.model_DRAEM import ReconstructiveSubNetwork, DiscriminativeSubNetwork
-from loss import FocalLoss, SSIM
 import os
 from torchvision import transforms
 import tqdm
 
-import torch.nn.functional as F
-import random
-
 import dataset.mvtec as mvtec
 from dataset.mvtec import MVTecDataset
+from models.model_DRAEM import ReconstructiveSubNetwork, DiscriminativeSubNetwork
+
 
 
 def get_lr(optimizer):
@@ -47,87 +42,80 @@ def get_data_transforms(size, isize):
         
         
         
-def DRAEM_train(args, dataloader, model, model_seg, loss_l2, loss_ssim, loss_focal, optimizer, visualizer, scheduler, run_name):
+def DRAEM_train(args):
     class_names = mvtec.CLASS_NAMES if args.class_name == 'all' else [args.class_name]
-    device = args.device
+    device = torch.device('cuda:{}'.format(args.gpu_id))
 
     for class_name in class_names:
-        best_img_roc = -1
-        best_pxl_roc = -1
-        best_pxl_pro = -1
+
         print(' ')
         print('%s | newly initialized...' % class_name)
         
         
-    train_dataset    = MVTecDataset(dataset_path  = args.data_path, 
-                                    class_name    =     class_name, 
-                                    resize        =            256,
-                                    cropsize      =      args.size,
-                                    is_train      =           True,
-                                    wild_ver      =        args.Rd)
-        
-    test_dataset     = MVTecDataset(dataset_path  = args.data_path, 
-                                    class_name    =     class_name, 
-                                    resize        =            256,
-                                    cropsize      =      args.size,
-                                    is_train      =          False,
-                                    wild_ver      =        args.Rd)
-
-
-    train_loader   = DataLoader(dataset         = train_dataset, 
-                                batch_size      =             1, 
-                                pin_memory      =          True,
-                                shuffle         =          True,
-                                drop_last       =          True,)
-
-    test_loader   =  DataLoader(dataset        =   test_dataset, 
-                                batch_size     =              1, 
-                                pin_memory     =           True,)
-    
-    
-    if args.cnn == 'wrn50_2':
-        model = wrn50_2(pretrained=True, progress=True)
-    elif args.cnn == 'res18':
-        model = res18(pretrained=True,  progress=True)
-    elif args.cnn == 'effnet-b5':
-        model = effnet.from_pretrained('efficientnet-b5')
-    elif args.cnn == 'vgg19':
-        model = vgg19(pretrained=True, progress=True)
-    elif args.cnn == 'DRAEM':
-        rgb2depthModel = DiscriminativeSubNetwork(in_channels=3, out_channels=1)
-        depth2rgbModel = DiscriminativeSubNetwork(in_channels=1, out_channels=3)
-        
-    rgb2depthModel, depth2rgbModel = rgb2depthModel.to(device), depth2rgbModel.to(device)
-        
-    loss_MSE = torch.nn.MSELoss()
-    learning_rate = args.lr
-    optimizer = torch.optim.Adam(list(rgb2depthModel.parameters())+list(depth2rgbModel.parameters()), lr=learning_rate)
-    
-    for epoch in tqdm(range(args.epochs), '%s -->'%(class_name)):
-
-        rgb2depthModel.train()
-        depth2rgbModel.train()
-        
-        loss_list = []
-        for (rgb, pc, depth, label, mask) in train_loader:
+        train_dataset    = MVTecDataset(dataset_path  = args.data_path, 
+                                        class_name    =     class_name, 
+                                        resize        =            256,
+                                        is_train      =           True)
             
-            
-            depthPred = rgb2depthModel(rgb.to(device))
-            rgbPred = depth2rgbModel(depth.to(device))
-            
-            loss = loss_MSE(rgb, rgbPred) + loss_MSE(depth, depthPred)
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        test_dataset     = MVTecDataset(dataset_path  = args.data_path, 
+                                        class_name    =     class_name, 
+                                        resize        =            256,
+                                        is_train      =          False)
 
-            loss_list.append(loss.item())
+
+        train_loader   = DataLoader(dataset         = train_dataset, 
+                                    batch_size      =             1, 
+                                    pin_memory      =          True,
+                                    shuffle         =          True,
+                                    drop_last       =          True,)
+
+        test_loader   =  DataLoader(dataset        =   test_dataset, 
+                                    batch_size     =              1, 
+                                    pin_memory     =           True,)
         
-        print('loss is:   ', np.mean(loss_list))
-        print('the radium of loss function is: ', loss_fn.r.item())
+        
+        if args.backbone == 'wrn50_2':
+            model = wrn50_2(pretrained=True, progress=True)
+        elif args.backbone == 'res18':
+            model = res18(pretrained=True,  progress=True)
+        elif args.backbone == 'effnet-b5':
+            model = effnet.from_pretrained('efficientnet-b5')
+        elif args.backbone == 'vgg19':
+            model = vgg19(pretrained=True, progress=True)
+        elif args.backbone == 'DRAEM':
+            rgb2depthModel = DiscriminativeSubNetwork(in_channels=3, out_channels=1)
+            depth2rgbModel = DiscriminativeSubNetwork(in_channels=1, out_channels=3)
+            
+        rgb2depthModel, depth2rgbModel = rgb2depthModel.to(device), depth2rgbModel.to(device)
+            
+        loss_MSE = torch.nn.MSELoss()
+        learning_rate = args.lr
+        optimizer = torch.optim.Adam(list(rgb2depthModel.parameters())+list(depth2rgbModel.parameters()), lr=learning_rate)
+        
+        for epoch in tqdm(range(args.epochs), '%s -->'%(class_name)):
 
-        torch.save(model.state_dict(), os.path.join(args.checkpoint_path, run_name+".pckl"))
-        torch.save(model_seg.state_dict(), os.path.join(args.checkpoint_path, run_name+"_seg.pckl"))
+            rgb2depthModel.train()
+            depth2rgbModel.train()
+            
+            loss_list = []
+            for (rgb, pc, depth, label, mask) in train_loader:
+                
+                
+                depthPred = rgb2depthModel(rgb.to(device))
+                rgbPred = depth2rgbModel(depth.to(device))
+                
+                loss = loss_MSE(rgb, rgbPred) + loss_MSE(depth, depthPred)
+                
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                loss_list.append(loss.item())
+            
+            print('epoch[{}/{}],  loss is: {} ',format(epoch, args.epochs, np.mean(loss_list)))
+
+            torch.save(rgb2depthModel.state_dict(), os.path.join(args.checkpoint_path, "rgb2depthModel.pckl"))
+            torch.save(depth2rgbModel.state_dict(), os.path.join(args.checkpoint_path, "depth2rgbModel.pckl"))
         
                 
         
@@ -140,26 +128,16 @@ if __name__=="__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--obj_id', default=1,  action='store', type=int)
     parser.add_argument('--bs', default = 8, action='store', type=int)
-    parser.add_argument('--lr', default=0.0001, action='store', type=float)
-    parser.add_argument('--epochs', default=700, action='store', type=int)
+    parser.add_argument('--lr', default=0.001, action='store', type=float)
+    parser.add_argument('--epochs', default=80, action='store', type=int)
     parser.add_argument('--gpu_id', default=0, action='store', type=int, required=False)
-    parser.add_argument('--data_path', default='/home/zhaoxiang/baselines/DRAEM/datasets/mvtec/', action='store', type=str)
-    parser.add_argument('--anomaly_source_path', default='/home/zhaoxiang/baselines/DRAEM/datasets/dtd/images/', action='store', type=str)
+    parser.add_argument('--data_path', default='/home/zhaoxiang/3D-ADS/datasets/mvtec3d', type=str)
     parser.add_argument('--checkpoint_path', default='./checkpoints/', action='store', type=str)
-    parser.add_argument('--log_path', default='./logs/', action='store', type=str)
-    parser.add_argument('--visualize', default=True, action='store_true')
-    parser.add_argument('--experiment_name', default='liver', choices=['retina, liver, brain, head'], action='store')
-    parser.add_argument('--backbone', default='noise', action='store')
+    parser.add_argument('--backbone', default='DRAEM', action='store')
+    parser.add_argument('--class_name', default='all', action='store')
     
-    # for noise autoencoder
-    parser.add_argument("-nr", "--noise_res", type=float, default=16,  help="noise resolution.")
-    parser.add_argument("-ns", "--noise_std", type=float, default=0.2, help="noise magnitude.")
-    parser.add_argument("-img_size", "--img_size", type=float, default=256, help="noise magnitude.")
-    parser.add_argument('--dataset_name', default='hist_DIY', choices=['hist_DIY', 'Brain_MRI', 'CovidX', 'RESC_average'], action='store')
-    parser.add_argument('--model', default='DRAEM_discriminitive', choices=['ws_skip_connection', 'DRAEM_reconstruction', 'DRAEM_discriminitive'], action='store')
-    parser.add_argument('--process_method', default='Guassian_noise', choices=['none', 'Guassian_noise', 'DRAEM'], action='store')
+    
     args = parser.parse_args()
 
     with torch.cuda.device(args.gpu_id):
